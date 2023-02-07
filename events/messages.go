93a799d01bdb46776c7ab0085d110186b005c41f
Cristian Morales <crisdavidmm95.dev@gmail.com>
2023-02-07 10:15:20 -0500

    nats configuration is done

--- events/messages.go
+++ events/messages.go
@@ -1,14 +1,16 @@
 package events
 
+import "time"
+
 type Message interface {
 	Type() string
 }
 
 type CreatedFeedMessage struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	CreatedAt   string `json:"created_at"`
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 func (m CreatedFeedMessage) Type() string {
